97f16f27543c6b51d70a6cd310934ae32f172267
shu_suzuki <ziroro@icloud.com>
2020-12-02 23:00:09 +0900

    deviseを導入しました

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  enum sex: { man: 0, woman: 1 }
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -4,10 +4,14 @@ RSpec.describe "StaticPages", type: :feature do
       visit root_path
     end
 
-    it 'リンクが表示されていること' do
+    it 'リンクまたはボタンが表示されていること' do
       expect(page).to have_link 'matching-ac'
-      expect(page).to have_link 'ログイン'
-      expect(page).to have_link '新規登録'
+      expect(page).to have_button '新規登録'
+      expect(page).to have_link '個人向け登録'
+      expect(page).to have_link '法人向け登録'
+      expect(page).to have_button 'ログイン'
+      expect(page).to have_link '個人向けログイン'
+      expect(page).to have_link '法人向けログイン'
       find('.home-icon').click
     end
 
@@ -23,6 +27,28 @@ RSpec.describe "StaticPages", type: :feature do
         find('.home-icon').click
       end
       expect(current_path).to eq root_path
+    end
+
+    it '個人向け登録,ログインページへ飛ぶこと' do
+      within(".nav") do
+        click_link '個人向け登録'
+      end
+      expect(current_path).to eq new_user_registration_path
+      within(".nav") do
+        click_link '個人向けログイン'
+      end
+      expect(current_path).to eq new_user_session_path
+    end
+
+    it '法人向け登録,ログインページへ飛ぶこと' do
+      within(".nav") do
+        click_link '法人向け登録'
+      end
+      expect(current_path).to eq new_company_registration_path
+      within(".nav") do
+        click_link '法人向けログイン'
+      end
+      expect(current_path).to eq new_company_session_path
     end
 
     it 'ページ内に特定の文字列が表示されていること' do
